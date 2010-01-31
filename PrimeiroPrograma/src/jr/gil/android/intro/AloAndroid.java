0f7ea360e0dfa958a04cb071c09fcbe9f79ff06a
giljr.2009@gmail.com <giljr.2009@gmail.com@c2d1672e-0e82-11df-9f54-17e475596acb>
2010-01-31 20:21:50 +0000

    Importacao inicial em 31/01/2010

---
+++ PrimeiroPrograma/src/jr/gil/android/intro/AloAndroid.java
@@ -0,0 +1,13 @@
+package jr.gil.android.intro;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AloAndroid extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
